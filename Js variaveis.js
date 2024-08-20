52cf6eceeebcc3e1debe2b5cb699292d6fe13d8d
nando244 <134401180+nando244@users.noreply.github.com>
2024-08-20 11:30:45 -0300

    Add files via upload

---
+++ Js variaveis.js
@@ -0,0 +1,61 @@
+//Variaveis
+//tipo Nunber 
+
+const meuNumero = 6;
+const numeroUm = 10;
+const numeroDois = 2;
+
+
+const soma = numeroDois+ meuNumero
+console.log(soma)
+
+const subtracao = numeroUm - meuNumero;
+console.log(subtracao);
+
+const multiplicacao = numeroDois * 10;
+console.log(multiplicacao);
+
+const divisao = numeroDois / numeroDois;
+console.log(divisao)
+
+//Float ( Número decimal )
+
+const numeroTres = 3.3;
+const numeroQuatro = .5 // mesma coisa que 0.5
+
+const operacaoM = numeroTres * numeroQuatro
+console.log(operacaoM)
+
+//Nam not a Number
+
+const operacaoN = 'a' * meuNumero
+console.log(operacaoN)
+
+const novaOperacao = 'a' + meuNumero
+console.log(novaOperacao)
+
+// string (texto)
+
+const meuNome = 'fernando'
+const frase = 'Meu nome e:'
+const issoNaoEumNumero = '50'
+
+const minhaFrase = meuNome + frase
+console.log(minhaFrase)
+
+const operacaoO = issoNaoEumNumero + meuNumero
+console.log(operacaoO);
+
+// Boolean
+// True -> verdadeiro;
+// false -> falso;
+
+const meuNumer = 6;  
+const comparacao = 6 === meuNumero;
+console.log(comparacao);
+
+const comparacaoII = '6' === meuNumero;
+console.log(comparacaoII); 
+
+const comparacaoIII = '6' === meuNumero; 
+console.log(comparacaoIII);
---
+++ README (1).md
@@ -0,0 +1,10 @@
+# Aula de fundamentos de JavaScript para back-end
+
+Projeto desenvolvimento  para compreenção dos conceitos básicos da linguagem de programção JavaScript
+
+#Aula 1 - 14/08/2024
+
+-Entender tipos de dados fundamentais;
+-Operações matemáticas;
+-Number, String e Boolean;
+-Algumas situações extras;
